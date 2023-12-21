7bf382e6ecf9458c7548fbb5449db9aa83d4ecfb
mhmdnurf <mhmd.nurf@gmail.com>
2023-12-22 03:34:09 +0700

    fix font color

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -300,6 +300,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '600',
     marginLeft: 10,
+    color: '#6F7789',
   },
   statusKekeruhan: {
     backgroundColor: 'white',
